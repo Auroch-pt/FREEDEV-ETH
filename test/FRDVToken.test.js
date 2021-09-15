8a474823d6eea15140bb37c2ddad94d7ea7e55e1
Aescudeiro <afonso_escudeiro@hotmail.com>
2021-09-15 18:25:39 +0000

    test: can transfer tokens between accounts

--- test/FRDVToken.test.js
+++ test/FRDVToken.test.js
@@ -14,4 +14,13 @@ contract("FRDVToken", (accounts) => {
       "Balance should be 1M tokens for contract creator"
     );
   });
+
+  it("can transfer tokens between accounts", async () => {
+    let amount = web3.utils.toWei("1000", "ether");
+    await frdvToken.transfer(accounts[1], amount, { from: accounts[0] });
+
+    let balance = await frdvToken.balanceOf(accounts[1]);
+    balance = web3.utils.fromWei(balance, "ether");
+    assert.equal(balance, "1000", "Balance should be 1K tokens");
+  });
 });
