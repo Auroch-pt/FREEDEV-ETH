a9bf6fe8ec76976f628f4c08d419fbe285b119f5
Aescudeiro <afonso_escudeiro@hotmail.com>
2021-09-19 00:39:51 +0000

    feat: create User and save it in platform

--- contracts/FRDVPlatform.sol
+++ contracts/FRDVPlatform.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./User.sol";
 
 contract FRDVPlatform {
 	IERC20 public token;
+	mapping(address => User) public users;
 
 	constructor(IERC20 _token) {
 		token = _token;
+	}
+
+	function createUser(string memory _username) public {
+		User storage newUser = users[msg.sender];
+		require(!newUser.set, "User already exists.");
+		users[msg.sender] = User({username: _username, set: true});
+	}
+
+	function getUser(address _address) public view returns (User memory){
+		return users[_address];
 	}
 }
