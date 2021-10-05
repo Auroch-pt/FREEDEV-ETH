6b4fe271b8a9f81e241473ff2f45b58b94a59701
Aescudeiro <afonso_escudeiro@hotmail.com>
2021-10-05 18:44:56 +0000

    refactor: convert issue struct to contract

--- contracts/Issue.sol
+++ contracts/Issue.sol
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-struct Issue {
+/* struct Issue {
 	address author;
 	uint256 id;
 	string title;
 	bool isSet;
 	uint256 reward;
+} */
+
+contract Issue {
+
+	address author;
+	uint256 id;
+	string title;
+	uint256 reward;
+
+	constructor(address _author, uint256 _id, string memory _title, uint256 _reward) {
+		author = _author;
+		id = _id;
+		title = _title;
+		reward = _reward;
+	}
+
 }
